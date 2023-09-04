cdce627b2f8142cee94450d90bb8add82068a80e
Шаронов Егор <142999567+Shaki0Ego@users.noreply.github.com>
2023-09-04 20:28:55 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "./module/DashboardLayout/DashboardLayout.";
+import { MainContent } from "./module/MainContent/MainContent";
+
+
+export function App() {
+    return <DashboardLayout>
+        <MainContent />
+    </DashboardLayout>
+
+}
