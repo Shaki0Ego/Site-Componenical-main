4b3535750f3953385e6162c9fd22fef1280cbc82
Шаронов Егор <142999567+Shaki0Ego@users.noreply.github.com>
2023-09-04 20:30:16 +0300

    Create Footer.jsx

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+
+const FooterStyled = styled.div`
+  color: white;
+  background-color: grey;
+  text-align: left;
+`
+
+
+export const Footer = () => {
+    return (<div>
+
+       
+        <FooterStyled>The Footer</FooterStyled>
+        
+    </div>)
+}
